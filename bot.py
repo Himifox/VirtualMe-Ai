e5769759fe98ff8dfde4db2c94e6d448c5860d50
苓织不是灵芝 <3461737415@qq.com>
2026-02-10 20:43:36 +0800

    释放8080端口

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
-ZHIPU_API ="14271215eb4e4a33a77ee2e2b81ffbb6.0e9UOV7rUBwUvVy6"
+# ZHIPU_API ="14271215eb4e4a33a77ee2e2b81ffbb6.0e9UOV7rUBwUvVy6"
 
 # 初始化 NoneBot
 nonebot.init()
@@ -14,5 +14,5 @@ nonebot.load_plugins("plugins")
 
 
 if __name__ == "__main__":
-    nonebot.run()
-
+    # 避免占用默认 8080 端口，改为 8081
+    nonebot.run(host="127.0.0.1", port=8081)
